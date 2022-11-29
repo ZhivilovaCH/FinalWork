c2c142c61ec1b3ea90ce1667f72049a17d147da8
Christina Zhivilova <krishakirova@yandex.ru>
2022-11-29 16:38:16 +0300

    Добавление решения задачи

---
+++ Task/Program.cs
@@ -0,0 +1,47 @@
+﻿string[] GetSplitArray(string array)
+{
+    string[] massive = array.Split(",");
+    return massive;
+}
+
+void GetPrint(string[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+string GetThreeElements(string[] massive)
+{
+
+    string massive2 = "";
+    for (int i = 0; i < massive.Length; i++)
+    {
+        if (massive[i].Length <= 3)
+        {
+            massive2+= massive[i] + " ";
+        }
+
+    }
+
+    return massive2;
+}
+
+Console.Clear();
+Console.WriteLine("Введите массив, состоящий из слов или цифр, через запятую: ");
+string array = Console.ReadLine();
+Console.WriteLine("Массив: ");
+string[] massive = GetSplitArray(array);
+GetPrint(massive);
+Console.WriteLine();
+string massive2 = GetThreeElements(massive);
+string[] massive3= GetSplitArray(massive2);
+Console.WriteLine("Длина меньше или равна трем символам: ");
+GetPrint(massive3);
+Console.WriteLine();
+
+
+
+
